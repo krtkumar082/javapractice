6c19d774d07b891400135b10f25371981edafcf0
krtkumar082 <kkumar2_be16@thapar.edu>
2020-09-21 14:47:58 +0530

    hello java

---
+++ src/main/java/com/Javapractice/App.java
@@ -0,0 +1,25 @@
+package com.Javapractice;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static final Logger LOG = LogManager.getLogger(App.class); 
+    public static void main( String[] args )
+    {
+    	String message="Hello,World";
+        LOG.debug(message + "Will Be Printed on Debug");
+        LOG.info(message + "Will Be Printed on Info");
+        LOG.warn(message + "Will Be Printed on Warn");
+        LOG.error(message + "Will Be Printed on error");
+        LOG.fatal(message + "Will Be Printed on fatal");
+        LOG.info("Appending String: {}." + message);
+        System.out.println(message);
+    }
+}
